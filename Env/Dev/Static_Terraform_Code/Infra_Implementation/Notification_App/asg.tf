ce3328dd136eb50e5cb2a4498a5fa9d6abcada7a
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-17 12:43:30 +0530

    Modify ASG configuration and add tags
    Updated the autoscaling group configuration to include additional tags and change health check type to ELB.
    
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
@@ -1,26 +1,53 @@
 resource "aws_autoscaling_group" "notification_asg" {
 
-  name = "${var.instance_name}-asg"
+  name                = "${var.instance_name}-asg"
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_capacity
 
-  min_size         = var.min_size
-  max_size         = var.max_size
-  desired_capacity = var.desired_capacity
+  vpc_zone_identifier = var.private_subnet_ids
 
-  vpc_zone_identifier = var.subnet_ids
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
 
   launch_template {
     id      = aws_launch_template.notification_lt.id
     version = "$Latest"
   }
 
-  health_check_type         = "EC2"
-  health_check_grace_period = 300
+  target_group_arns = [
+    var.target_group_arn
+  ]
 
   force_delete = true
 
   tag {
     key                 = "Name"
     value               = var.instance_name
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Application"
+    value               = var.application
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = var.environment
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Owner"
+    value               = var.owner
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "CostCenter"
+    value               = var.cost_center
     propagate_at_launch = true
   }
 }
