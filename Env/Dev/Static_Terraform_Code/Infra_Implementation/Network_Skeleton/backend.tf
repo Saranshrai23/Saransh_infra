c6642fb183663b72e6228431f26a19a530a27ee5
Saransh Rai <saranshrai07@gmail.com>
2026-07-21 01:45:57 +0530

    Update backend.tf

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "otms-terraform-state-dev"
+    bucket       = "otms-terraform-state-dev-788572613316"
     key          = "route53.tfstate"
     region       = "us-east-1"
     encrypt      = true
